e83e72b4c982fe3496dc1fff6f53a5fcc0cad21e
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-09-10 10:58:48 +0200

    adding tests to pipeline

---
+++ src/utils/sortByVolume/sortByVolume.test.ts
@@ -0,0 +1,50 @@
+// sum.test.js
+import { expect, test } from 'vitest';
+
+import { sortByVolume } from './sortByVolume.js';
+
+test('sorting bins', () => {
+  const sortedBins = sortByVolume([
+    {
+      name: 'BinOne',
+      size: {
+        width: 20,
+        height: 20,
+        depth: 20,
+      },
+      items: [],
+    },
+    {
+      name: 'BinTwo',
+      size: {
+        width: 20,
+        height: 10,
+        depth: 20,
+      },
+      items: [],
+    },
+    {
+      name: 'BinThree',
+      size: {
+        width: 400,
+        height: 10,
+        depth: 20,
+      },
+      items: [],
+    },
+    {
+      name: 'BinFour',
+      size: {
+        width: 20,
+        height: 10,
+        depth: 20,
+      },
+      items: [],
+    },
+  ]);
+
+  expect(sortedBins[0].name).toBe('BinThree');
+  expect(sortedBins[1].name).toBe('BinOne');
+  expect(sortedBins[2].name).toBe('BinTwo');
+  expect(sortedBins[3].name).toBe('BinFour');
+});
--- src/utils/sortByVolume/sortByVolume.ts
+++ src/utils/sortByVolume/sortByVolume.ts
@@ -1,6 +1,23 @@
 import { Bin } from '@types';
 
-export const sortByVolume = (bins: Bin[]) => {
-  // TODO: sort small to large
-  return bins;
+/**
+ * Sorts by volume in descending order
+ */
+export const sortByVolume = (items: Bin[]) => {
+  return items.toSorted((itemOne: Bin, itemTwo: Bin) => {
+    const itemOneVolume =
+      itemOne.size.width * itemOne.size.height * itemOne.size.depth;
+    const itemTwoVolume =
+      itemTwo.size.width * itemTwo.size.height * itemTwo.size.depth;
+
+    if (itemOneVolume > itemTwoVolume) {
+      return -1;
+    }
+
+    if (itemOneVolume < itemTwoVolume) {
+      return 1;
+    }
+
+    return 0;
+  });
 };
