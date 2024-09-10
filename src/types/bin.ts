31025a2d9b8920ece57d23c72ce2f3587642766b
Tobias Herlitz <61463464+TobiasHerlitz@users.noreply.github.com>
2024-09-10 11:04:57 +0200

    adding tests to pipeline (#4)
    adding tests to pipeline

--- src/types/bin.ts
+++ src/types/bin.ts
@@ -4,6 +4,5 @@ import { PlacedParcel } from './parcel';
 export interface Bin {
   name: string;
   size: Size;
-  maxWeight: number;
   items: PlacedParcel[];
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults as testDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +16,11 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     host: true, // Needed to run in docker
+  },
+  test: {
+    coverage: {
+      reporter: ['text'], // Only report in cli
+      exclude: [...testDefaults.exclude, '**/index.ts'],
+    },
   },
 });
